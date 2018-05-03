e248663b6bb1ed8fa22909979c065c9b3a22f603
rpgowers <r.gowers@warwick.ac.uk>
2018-05-03 17:10:34 +0100

    Edited test.py to save the population data.

--- test.py
+++ test.py
@@ -1,12 +1,33 @@
+import mole_sim
 from mole_sim import Colony
+import numpy as np
+import sys
 
 col = Colony()
 col.initialise_colony()
-for t in range(0,7000):
+N = int(sys.argv[1]) # typically N=20
+T = int(sys.argv[2]) # 1 is a quarter
+col.pop_capacity = N
+
+cells = np.zeros([N,T])-1
+food = np.zeros(T)
+
+for t in range(0,T):
     col.update_population()
     col.remove_dead()
     col.consume_food()
     col.remove_dead()
     col.ensure_queen()
+    food[t] = col.food_stockpile
+    for i in range(0,len(col.population)):
+    	cells[i,t] = mole_sim.custom_sort(col.population[i])
 
-print(col)
+import matplotlib.pylab as plt
+
+plt.imshow(cells,aspect='auto')
+plt.show()
+
+plt.plot(range(T),food)
+plt.show()
+
+np.savetxt('population_history.txt',cells,fmt='%i')
